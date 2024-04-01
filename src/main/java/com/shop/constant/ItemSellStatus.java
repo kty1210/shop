a84e4f0d08342cd4264d0ea34e01baa242cf9045
kty <kwonabraham@naver.com>
2024-04-01 14:15:58 +0900

    Querydsl 작성완료

--- src/main/java/com/shop/constant/ItemSellStatus.java
+++ src/main/java/com/shop/constant/ItemSellStatus.java
@@ -1,6 +1,5 @@
 package com.shop.constant;
 
-// enum = 여기 외 다른값을 넣지 마라
 public enum ItemSellStatus {
     SELL, SOLD_OUT
 }
--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -7,39 +7,37 @@ import lombok.*;
 import java.time.LocalDateTime;
 
 @Entity
-
-//테이블명 설정해주는 annotation
-//default 값 = class 명
-@Table(name="item")
 @Getter
-@ToString
 @Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Item {
 
     @Id
-    @Column(name="item_id") // 데이터베이스 열과 매핑
-    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto-increment
-    private Long id;            //상품코드
+    @Column(name="item_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;          //상품 코드
 
     @Column(nullable = false, length = 50)
-    private String itemNm;      //상품명 , sql은 대소문자를 구분 안해서 스네이크표기법으로 바뀜
+    private String itemNm;  // 상품명
 
     @Column(nullable = false)
-    private int price;          //가격
+    private int price;      // 가격
 
     @Column(nullable = false)
-    private int stockNumber;    //재고수량
+    private int stockNumber; // 재고수량
 
-    @Lob // 다량의 데이터 저장시 사용
+    @Lob
     @Column(nullable = false)
-    private String itemDetail;     // 상품 상세 설명
+    private String itemDetail; // 상품 상세 설명
 
     @Enumerated(EnumType.STRING)
-    private ItemSellStatus itemSellStatus;     // 상품 판매 상태
-    
-    private LocalDateTime regTime; // 등록시간
-    
-    private LocalDateTime updateTime; // 수정시간
+    private ItemSellStatus itemSellStatus; // 상품 판매 상태
 
+    private LocalDateTime regTime; // 등록 시간
+
+    private LocalDateTime updateTime; // 수정 시간
 
 }
